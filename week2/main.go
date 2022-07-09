6b66ecfabe442c82a15519c59a7d8b744ff2e22a
CCD <ccd1020@yeah.net>
2022-07-09 19:42:22 +0800

    modify main.go

--- week2/main.go
+++ week2/main.go
@@ -1,43 +1,43 @@
 package main
 
-import(
-	"github.com/pkg/errors"
+import (
 	"database/sql"
 	"fmt"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
 )
 
-func test()(string, error){
+func test() (string, error) {
 	var email string
-	db, err := sql.Open("mysql","root:123456@tcp(49.235.94.123:13306)/wordpress?charset=utf8")	
-	if err != nil{
-	//	err = errors.WithStack(err)
-		err = errors.Wrap(err,"message my test")
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/wordpress?charset=utf8")
+	if err != nil {
+		//	err = errors.WithStack(err)
+		err = errors.Wrap(err, "message my test")
 		//	fmt.Print(err)
 		return "", err
 	}
 	rows, err := db.Query("SELECT user_email FROM wp_users where id = 9 ")
-	 if err != nil{
-		 err = errors.Wrap(err,"db message")
+	if err != nil {
+		err = errors.Wrap(err, "db message")
 		//////////////// fmt.Print(err)
-	         return "", err
-	 }
- 	 err =  rows.Scan(&email)
-	 if err != nil{
-	 	err = errors.Wrap(err, "未查询到")
 		return "", err
-	 }
-	 return email, nil
+	}
+	err = rows.Scan(&email)
+	if err != nil {
+		err = errors.Wrap(err, "未查询到")
+		return "", err
+	}
+	return email, nil
 }
 
-func main(){
+func main() {
 
 	ret, err := test()
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
-		return 
+		return
 	}
 	fmt.Print(ret)
 
 }
-
